d624bd121faeca475f94c7553e3ade1e39766649
MOONcitizenX <mooncitizenx@gmail.com>
2023-03-28 20:43:18 +0300

    config: remove unused files, configure eslint, prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: { target: browserslistToEsbuild(['>0.2%', 'not dead', 'not op_mini all']) },
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@public': path.resolve(__dirname, './public'),
+    },
+  },
+});
