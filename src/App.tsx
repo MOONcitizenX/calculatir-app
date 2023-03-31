c10ddb0fe5f8872dc38fd774ba78ef402b620b61
MOONcitizenX <mooncitizenx@gmail.com>
2023-03-31 23:14:51 +0300

    refactor: remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import ErrorBoundary from '@components/ErrorBoundary/ErrorBoundary';
 import Router from '@router/Router';
 
 const App = () => {
-  return (
-    <ErrorBoundary fallback={<div>Something went wrong</div>}>
-      <Router />
-    </ErrorBoundary>
-  );
+  return <Router />;
 };
 
-// ErrorBoundary => Provider => ThemeProvider => Router
 export default App;
